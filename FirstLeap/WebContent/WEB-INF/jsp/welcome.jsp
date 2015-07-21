ef194c9a10a42c4c8cc0010813b1c813f698dcf3
Ayush <ayushr@amazon.com>
2015-07-22 00:33:42 +0530

    Basic Setup

---
+++ FirstLeap/WebContent/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>First Spring MVC</title>
+<style type="text/css">
+body {
+	background-image: url('http://crunchify.com/bg.png');
+}
+</style>
+</head>
+<body>${message}
+ 
+	<br>
+	<br>
+	<div style="font-family: verdana; padding: 10px; border-radius: 10px; font-size: 12px; text-align:center;">
+ 
+		
+	</div>
+</body>
+</html>
---
+++ FirstLeap/src/com/firstleap/controller/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.firstleap.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/*
+ * author: Ayush Rajoria
+ * date : 21 July, 2015
+ */
+ 
+@Controller
+public class HelloWorld {
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from HelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
